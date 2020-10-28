6a4a9d70b5a097cba2010d5821412afa770ba4e1
srisahiti <71134221+srisahiti@users.noreply.github.com>
2020-10-28 17:29:40 +0530

    Add files via upload

---
+++ particle.js
@@ -0,0 +1,25 @@
+class Particle {
+    constructor(x,y){
+        var options={
+            restitution:0.3,
+            friction:0.5,
+            density:1.2
+        }
+        this.body=Bodies.circle(x,y,10,options)
+        this.radius=10
+        World.add(world,this.body)
+    }
+    
+    
+    display(){
+        var pos = this.body.position
+        var angle = this.body.angle
+        push()
+        translate(pos.x,pos.y)
+        rotate(angle)
+        ellipseMode(RADIUS)
+        fill("red");
+        ellipse(0,0,this.radius,this.radius)
+        pop();
+    }
+}
---
+++ plinko.js
@@ -0,0 +1,23 @@
+class Plinko {
+    constructor(x,y){
+        var options={
+            isStatic:true,
+        }
+        this.body=Bodies.circle(x,y,10,options)
+        this.radius=10
+        World.add(world,this.body)
+    }
+    
+    
+    display(){
+        var pos = this.body.position
+        var angle = this.body.angle
+        push()
+        translate(pos.x,pos.y)
+        rotate(angle)
+        ellipseMode(RADIUS)
+        fill("red");
+        ellipse(0,0,this.radius,this.radius)
+        pop();
+    }
+}
